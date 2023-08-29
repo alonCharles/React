3ff463f20cc21fff2149bee45d6407754f959994
alonCharles <aloncharlesdev@gmail.com>
2023-08-29 13:30:14 -0400

    update

---
+++ meals/src/components/Meals.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+import { AppContext } from "../context"
+
+
+const Meals = () => {
+const context = useContext(AppContext)
+console.log(context)
+    return(
+        <>
+            <h1>Meals</h1>
+        </>
+    )
+
+}
+
+export default Meals
---
+++ meals/src/components/Modal.jsx
@@ -0,0 +1,11 @@
+const Modal = () => {
+    
+    return(
+        <>
+            <h1>Modal</h1>
+        </>
+    )
+
+}
+
+export default Modal
